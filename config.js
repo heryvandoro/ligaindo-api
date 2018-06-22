800b6c5c1fffffcfb1f2070ed295db83be8ca8c5
Hery Vandoro <vandorohery99@gmail.com>
2018-06-22 10:17:31 +0700

    Install dotenv, add detail club route

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    BASE_ENDPOINT : "https://liga-indonesia.id"
+    BASE_ENDPOINT : "https://liga-indonesia.id",
+    BASE_URL : `http://localhost:${process.env.port || 3000}`
 }
--- controllers/ClubController.js
+++ controllers/ClubController.js
@@ -1,9 +1,58 @@
 const router = require('express').Router();
 const config = require("../config");
+const Crawler = require("crawler");
+
+const crawler = new Crawler({
+    jQuery : true
+});
 
 router.get("/", async (req, res) => {
-    
-    res.sendStatus(200);
+    await crawler.direct({
+        uri : `${config.BASE_ENDPOINT}/klub`,
+        callback : (error, data) => {
+            if(error) return res.sendStatus(503);
+            const $ = data.$;
+            let clubs = [];
+            let clubs_dom = $(".container .card--club .wrapper");
+            clubs_dom.each((i, club) => {
+                club = $(club);
+                let slug = club.find("a").eq(0).attr("href").split("/");
+                slug = slug[slug.length-1];
+                let club_data = {
+                    name : club.find(".club-title").text(),
+                    stadion : club.find(".title-place").text(),
+                    city : club.find(".place-name").text(),
+                    image : club.find("img").attr("src"),
+                    detail_url : `${config.BASE_URL}/clubs/${slug}`
+                };
+                clubs.push(club_data);
+            });
+            res.send(clubs);
+        }
+    });
+});
+
+router.get("/:club_name", async (req, res) => {
+    await crawler.direct({
+        uri : `${config.BASE_ENDPOINT}/klub/${req.params.club_name}`,
+        callback : (error, data) => {
+            if(error) return res.sendStatus(503);
+            const $ = data.$;
+            let clubs = [];
+            let clubs_dom = $(".container .card--club .wrapper");
+            clubs_dom.each((i, club) => {
+                club = $(club);
+                let club_data = {
+                    name : club.find(".club-title").text(),
+                    stadion : club.find(".title-place").text(),
+                    city : club.find(".place-name").text(),
+                    image : club.find("img").attr("src")
+                };
+                clubs.push(club_data);
+            });
+            res.send(clubs);
+        }
+    });
 });
 
 module.exports = router;
